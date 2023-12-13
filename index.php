219e85c8f40421fbf84b789871d5d2a8144576e4
drew-chase <dcmanproductions@gmail.com>
2023-12-13 10:55:40 -0500

    added default styling

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
-
+<link rel="stylesheet" href="/assets/css/main.min.css">
     <!-- Font Awesome -->
     <link rel="stylesheet" href="/assets/lib/fontawesome/css/all.min.css">
 
