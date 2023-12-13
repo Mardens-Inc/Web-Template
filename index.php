1e8bd5635a5ed30ac2b9c73327411b366d35dd3a
drew-chase <dcmanproductions@gmail.com>
2023-12-13 09:00:41 -0500

    Add Font Awesome stylesheet

--- index.php
+++ index.php
@@ -4,6 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="/assets/lib/">
+
 </head>
 <body>
     <h1>Hello World</h1>
